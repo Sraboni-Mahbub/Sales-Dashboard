eb963f831dbcabc678e10d3cdd7ad794dc2ff70f
sraboni mahbub <sraboni.mahbub@gmail.com>
2023-10-15 10:53:54 +0600

    modfied

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -10,9 +10,16 @@ urlpatterns = [
     path('update-user-info/<int:user_id>/', views.update_user_info, name='update_user_info'),
     path('view-user/<int:user_id>', views.view_user, name='view_user'),
 
-    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="authenticate/reset_password.html"), name="reset_password"),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="authenticate/reset_password.html"),
+         name="reset_password"),
+    path('reset_password_sent/',
+         auth_views.PasswordResetDoneView.as_view(template_name="authenticate/reset_password_sent.html"),
+         name="password_reset_done"),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name="authenticate/password_reset_confirm.html"),
+         name="password_reset_confirm"),
+    path('reset_password_complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name="authenticate/password_reset_complete.html"),
+         name="password_reset_complete"),
 
 ]
