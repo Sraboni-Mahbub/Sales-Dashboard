ceb54a9a016f032f3795231f3c23f8c1ffdec3d5
sraboni mahbub <sraboni.mahbub@gmail.com>
2023-10-15 16:26:16 +0600

    Added

--- homedash/admin.py
+++ homedash/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from homedash.models import *
 
-# Register your models here.
 
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('p_name', 'sales_category', 'price')
+
+class SalesAdmin(admin.ModelAdmin):
+    list_display = ('display_product_names', 'user_profile', 'sale_value', 'remarks', 'date')
+
+    def display_product_names(self, obj):
+        product_names = ', '.join([product.p_name for product in obj.product.all()])
+        return product_names
+
+    display_product_names.short_description = 'Products'
+
+class LogTableAdmin(admin.ModelAdmin):
+    list_display = ('user', 'actions', 'results', 'dateTime', 'description')
+
+
+admin.site.register(Products, ProductsAdmin)
+admin.site.register(Sale, SalesAdmin)
+admin.site.register(LogTable, LogTableAdmin)
 
--- homedash/models.py
+++ homedash/models.py
@@ -1,1 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
+from authenticate.models import UserProfile, SalesCategory
+
+
+
+class Products(models.Model):
+    sales_category = models.ForeignKey(SalesCategory, on_delete=models.CASCADE,  related_name="sales_category_products",
+                                       blank=True, null=True)
+    p_name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.p_name
+
+class Sale(models.Model):
+    product = models.ManyToManyField(Products)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    sale_value = models.DecimalField(max_digits=10, decimal_places=2)
+    remarks = models.CharField(max_length=50)
+    date = models.DateTimeField()
+
+    def __str__(self):
+        product_names = ', '.join([product.p_name for product in self.product.all()])
+        return f"{product_names}"
+
+class LogTable(models.Model):
+    user = models.CharField(max_length=50)
+    actions = models.CharField(max_length=50)
+    results = models.CharField(max_length=50)
+    dateTime = models.DateTimeField()
+    description = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.user
+
